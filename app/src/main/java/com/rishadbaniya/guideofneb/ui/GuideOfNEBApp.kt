f933949e1300891e76803911165daf27c0823548
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-08 16:55:27 +0545

    Added GuideOfNEBApp.kt

---
+++ app/src/main/java/com/rishadbaniya/guideofneb/ui/GuideOfNEBApp.kt
@@ -0,0 +1,17 @@
+package com.rishadbaniya.guideofneb.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.rishadbaniya.guideofneb.ui.theme.GuideOfNEBTheme
+
+@Composable
+fun GuideofNEBApp(){
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(GuideOfNEBTheme.colors.background)){
+
+    }
+}
--- app/src/main/java/com/rishadbaniya/guideofneb/ui/MainActivity.kt
+++ app/src/main/java/com/rishadbaniya/guideofneb/ui/MainActivity.kt
@@ -3,8 +3,15 @@ package com.rishadbaniya.guideofneb.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import com.rishadbaniya.guideofneb.ui.theme.GuideOfNEBTheme
 
 class MainActivity : ComponentActivity() {
@@ -14,8 +21,13 @@ class MainActivity : ComponentActivity() {
             GuideOfNEBTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = GuideOfNEBTheme.colors.background) {
+                    GuideofNEBApp()
                 }
             }
         }
     }
 }
+
+
+
+

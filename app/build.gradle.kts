d06875a482348c838fb65429b44424c80467e7c2
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-10 20:24:11 +0545

    Added accompanist system ui controller

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
     implementation("androidx.activity:activity-compose:1.3.0-beta01")
     implementation("com.google.dagger:hilt-android:2.36")
+    implementation ("com.google.accompanist:accompanist-systemuicontroller:0.11.1")
     kapt("com.google.dagger:hilt-android-compiler:2.36")
     implementation("androidx.navigation:navigation-compose:2.4.0-alpha02")
     testImplementation("junit:junit:4.+")

e3d7233eff6175070c8ae04cdc4e858bb5e97aed
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-10 16:34:35 +0545

    removed unwanted assets

--- app/src/main/java/com/rishadbaniya/guideofneb/ui/assets.kt
+++ app/src/main/java/com/rishadbaniya/guideofneb/ui/assets.kt
@@ -14,10 +14,3 @@ const val NEWS_LINE = R.drawable.newspaper_line
 const val NEWS_FILL = R.drawable.newspaper_fill
 const val STACK_LINE = R.drawable.stack_line
 const val STACK_FILL = R.drawable.stack_fill
-
-const val LABEL_HOME = R.drawable.label_home;
-const val LABEL_NOTES = R.drawable.label_notes;
-const val LABEL_SOLUTIONS = R.drawable.label_solutions;
-const val LABEL_TOOLS = R.drawable.label_tools;
-const val LABEL_NEWS = R.drawable.label_news;
-const val LABEL_LIBRARY = R.drawable.label_library;

4e9674e9c05c9455ec01ff98d87f654b99c15b48
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-10 16:33:04 +0545

    added sans serif pro font family

---
+++ app/src/main/java/com/rishadbaniya/guideofneb/ui/theme/Fonts.kt
@@ -0,0 +1,15 @@
+package com.rishadbaniya.guideofneb.ui.theme
+
+import androidx.compose.ui.text.font.Font;
+import androidx.compose.ui.text.font.FontFamily;
+import androidx.compose.ui.text.font.FontWeight
+import com.rishadbaniya.guideofneb.R;
+
+val SOURCE_SAN_PRO = FontFamily(
+   Font(R.font.source_sans_pro_light, FontWeight.Light),
+   Font(R.font.source_sans_pro_bold, FontWeight.Bold),
+   Font(R.font.source_sans_pro_semibold, FontWeight.SemiBold),
+   Font(R.font.source_sans_pro_normal, FontWeight.Normal),
+   Font(R.font.source_sans_pro_black, FontWeight.Black),
+   Font(R.font.source_sans_pro_extralight, FontWeight.ExtraLight),
+)

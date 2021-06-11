fe0531f65a0a3e2e8cc7ebd151d9011100eca5b0
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-11 17:08:14 +0545

    Added splash themme

--- app/src/main/java/com/rishadbaniya/guideofneb/ui/MainActivity.kt
+++ app/src/main/java/com/rishadbaniya/guideofneb/ui/MainActivity.kt
@@ -3,20 +3,14 @@ package com.rishadbaniya.guideofneb.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.rishadbaniya.guideofneb.ui.theme.GuideOfNEBTheme
+import com.rishadbaniya.guideofneb.R
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_GuideOfNEB_NoActionBar)
         setContent {
             GuideOfNEBTheme {
                 // A surface container using the 'background' color from the theme

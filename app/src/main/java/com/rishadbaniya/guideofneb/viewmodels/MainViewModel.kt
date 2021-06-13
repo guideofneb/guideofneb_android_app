d03069f8e636f02ab58e52e48a3884c7305c39a5
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-13 14:19:43 +0545

    added app drawer for whole app

--- app/src/main/java/com/rishadbaniya/guideofneb/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/rishadbaniya/guideofneb/viewmodels/MainViewModel.kt
@@ -1,15 +1,33 @@
 package com.rishadbaniya.guideofneb.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.util.Log
+import androidx.compose.material.DrawerState
+import androidx.compose.material.DrawerValue
+import androidx.compose.material.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.lifecycle.*
 import com.rishadbaniya.guideofneb.data.AppDataStore
 import com.rishadbaniya.guideofneb.model.Theme
+import kotlinx.coroutines.channels.BroadcastChannel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.produceIn
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(
-    private val appDataStore: AppDataStore
+class MainViewModel constructor(
+//    private val appDataStore: AppDataStore
 ) : ViewModel(){
-    val getTheme : Flow<Theme> get() = appDataStore.getTheme
 
-    val setTheme : (Theme) -> Unit get() = appDataStore.setTheme
+    val openDrawer : () -> Unit = {
+        viewModelScope.launch {
+            drawerChannel.send(DrawerValue.Open)
+        }
+    }
+
+    private val drawerChannel : BroadcastChannel<DrawerValue> = BroadcastChannel<DrawerValue>(1);
+    val drawerActions : LiveData<DrawerValue> = drawerChannel.asFlow().asLiveData()
 }
+
+val LocalMainViewModel = staticCompositionLocalOf<MainViewModel>{error("MainViewModel was not provided")}

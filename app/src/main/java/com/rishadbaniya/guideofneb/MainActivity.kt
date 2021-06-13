0f3121c0e7f62401f417dd8de6e2b17664cf685e
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-13 14:21:45 +0545

    Updated UI for view model

---
+++ app/src/main/java/com/rishadbaniya/guideofneb/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.rishadbaniya.guideofneb
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.viewModels
+import androidx.compose.material.Surface
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.rishadbaniya.guideofneb.ui.theme.GuideOfNEBTheme
+import com.rishadbaniya.guideofneb.ui.GuideofNEBApp
+import com.rishadbaniya.guideofneb.viewmodels.LocalMainViewModel
+import com.rishadbaniya.guideofneb.viewmodels.MainViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlin.time.ExperimentalTime
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    private val mainViewModel by viewModels<MainViewModel>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_GuideOfNEB_NoActionBar)
+        setContent {
+            val navController = rememberNavController()
+            GuideOfNEBTheme{
+                // A surface container using the 'background' color from the theme
+                Surface(color = GuideOfNEBTheme.colors.background) {
+                    CompositionLocalProvider(LocalMainViewModel provides mainViewModel) {
+                        GuideofNEBApp(navController = navController)
+                    }
+
+                }
+            }
+        }
+    }
+}
+
+
+
+
--- app/src/main/java/com/rishadbaniya/guideofneb/ui/GuideOfNEBApp.kt
+++ app/src/main/java/com/rishadbaniya/guideofneb/ui/GuideOfNEBApp.kt
@@ -1,36 +1,75 @@
 package com.rishadbaniya.guideofneb.ui
 
+import android.annotation.SuppressLint
+import android.util.Log
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import com.rishadbaniya.guideofneb.BaseApplication
+import com.rishadbaniya.guideofneb.MainActivity
+import com.rishadbaniya.guideofneb.ui.components.APP_DRAWER
 import com.rishadbaniya.guideofneb.ui.components.BottomNavigationBar
+import com.rishadbaniya.guideofneb.ui.screens.SETTING
 import com.rishadbaniya.guideofneb.ui.screens.tools.TOOLS
+import com.rishadbaniya.guideofneb.ui.theme.GuideOfNEBTheme
+import com.rishadbaniya.guideofneb.viewmodels.LocalMainViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import com.rishadbaniya.guideofneb.ui.components.APP_BAR as AppBar
 
+object ROUTES {
+    const val HOME = "home"
+    const val NOTES = "notes"
+    const val SOLUTIONS = "solutions"
+    const val TOOLS = "tools"
+    const val NEWS = "news"
+    const val LIBRARY = "library"
+    const val SETTINGS = "settings"
+}
+
+private val ROUTES_TO_SHOW_BOTTOM_NAV = arrayOf(
+    ROUTES.HOME,
+    ROUTES.NOTES,
+    ROUTES.TOOLS,
+    ROUTES.SOLUTIONS,
+    ROUTES.TOOLS,
+    ROUTES.NEWS,
+    ROUTES.LIBRARY
+);
+
+
+@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
-fun GuideofNEBApp(){
-    val navController = rememberNavController();
-    Column {
-        AppBar()
-        Body(modifier = Modifier.weight(1f), navController = navController)
-        BottomNavigationBar(
-            navController = navController,
-            showBannerAd = false
-        )
+fun GuideofNEBApp(
+    navController: NavHostController
+){
+    APP_DRAWER(navController = navController) {
+        Column {
+            Body(modifier = Modifier.weight(1f), navController = navController)
+            BottomNavigationBar(
+                navController = navController,
+                routesToShowBottomNav = ROUTES_TO_SHOW_BOTTOM_NAV
+            )
+        }
     }
 }
 
 @Composable
 fun Body(
-   modifier : Modifier,
+    modifier : Modifier,
    navController : NavHostController
 ){
-    NavHost(navController = navController, startDestination = "tools", modifier = modifier){
+    NavHost(navController = navController, startDestination = "home", modifier = modifier){
         composable("home"){
         }
         composable("notes"){
@@ -38,11 +77,23 @@ fun Body(
         composable("solutions"){
         }
         composable("tools"){
-            TOOLS()
+           Column{
+               AppBar(
+                   showLogo = true,
+                   navController = navController,
+                   showDrawer = true
+               )
+               TOOLS(modifier = Modifier.weight(1f))
+           }
         }
+
         composable("news"){
         }
-        composable("library"){
+        composable(ROUTES.LIBRARY){
+
+        }
+        composable(ROUTES.SETTINGS){
+            SETTING(navController = navController)
         }
     }
 }

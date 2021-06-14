fc44a5defe6031381ac41e6dbbb546267c912388
Rishad Baniya <54678051+expropace@users.noreply.github.com>
2021-06-14 08:26:58 +0545

    Used app drawer in the app

--- app/src/main/java/com/rishadbaniya/guideofneb/ui/App.kt
+++ app/src/main/java/com/rishadbaniya/guideofneb/ui/App.kt
@@ -1,30 +1,17 @@
 package com.rishadbaniya.guideofneb.ui
 
 import android.annotation.SuppressLint
-import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
-import androidx.lifecycle.Observer
-import androidx.lifecycle.asLiveData
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import com.rishadbaniya.guideofneb.BaseApplication
-import com.rishadbaniya.guideofneb.MainActivity
-import com.rishadbaniya.guideofneb.ui.components.APP_DRAWER
+import com.rishadbaniya.guideofneb.ui.components.AppDrawer
 import com.rishadbaniya.guideofneb.ui.components.BottomNavigationBar
 import com.rishadbaniya.guideofneb.ui.screens.SETTING
 import com.rishadbaniya.guideofneb.ui.screens.tools.TOOLS
-import com.rishadbaniya.guideofneb.ui.theme.GuideOfNEBTheme
-import com.rishadbaniya.guideofneb.viewmodels.LocalMainViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.asFlow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
 import com.rishadbaniya.guideofneb.ui.components.APP_BAR as AppBar
 
 object ROUTES {
@@ -48,12 +35,11 @@ private val ROUTES_TO_SHOW_BOTTOM_NAV = arrayOf(
 );
 
 
-@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun GuideofNEBApp(
     navController: NavHostController
 ){
-    APP_DRAWER(navController = navController) {
+    AppDrawer(navController = navController) {
         Column {
             Body(modifier = Modifier.weight(1f), navController = navController)
             BottomNavigationBar(
